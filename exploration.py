3bbf2c995b57e42afc624f747a592bac368d6dec
kktsch <haldun.koktas@gmail.com>
2022-10-05 12:35:38 +0300

    Update exploration.py

--- exploration.py
+++ exploration.py
@@ -8,7 +8,7 @@ numerical_columns = world_population_data.select_dtypes(include=["float64", "int
 sns.set(rc={'figure.figsize':(11.7,8.27)})
 sns.heatmap(numerical_columns.corr(), cmap= 'coolwarm', vmin=-1, vmax=1, center= 0)
 
-# Safety vs IQ
+# Safety vs IQ (put any variable here to see its plot)
 sns.set(font_scale = 1)
 #sns.scatterplot(x = "iq", y = "safety", data = world_population_data).set(title='Safety vs life Expectancy')
 sns.regplot(x = "iq", y = "safety", data=world_population_data).set(title='Safety vs IQ')
@@ -16,5 +16,5 @@ sns.regplot(x = "iq", y = "safety", data=world_population_data).set(title='Safet
 #Education Expenditure Boxplot
 world_population_data.boxplot("education_expenditure_per_inhabitant")
 
-#Distributions
+#Distributions (put any variable to see its distibution)
 sns.histplot(world_population_data.rights, kde = True).set(title='Number of Countries According to Rights Score')
